8dc75b87519b98be7fe9fb812dac9dde2766e48c
Mehul Kar <mehul.kar@apple.com>
2016-01-25 10:06:44 -0800

    more stuff

--- app/controllers/votes_controller.rb
+++ app/controllers/votes_controller.rb
@@ -1,16 +1,37 @@
 class VotesController < ApplicationController
   def create
-    if params[:vote][:person_name].present?
-      params[:vote][:locations].map do |key, val|
+    if person_name.present?
+      location_params.map do |key, val|
         @poll.votes.find_or_create_by(
-          person_name: params[:vote][:person_name],
+          person_name: person_name,
           location_id: key.to_i
         )
       end
+
+      new_location_params.map do |location|
+        new_location = Location.create(name: location)
+        @poll.votes.find_or_create_by(
+          person_name: person_name,
+          location_id: new_location.id
+        )
+      end
+
       session[:voted] = true
       redirect_to root_path, notice: 'Thanks for voting!'
     else
-      redirect_to root_path, notice: 'I said... wass yo name!'
+      redirect_to root_path, alert: "Vote was not recorded becuase you didn't put your name. Try again plz kthx"
     end
+  end
+
+  def location_params
+    params[:vote].fetch(:locations, [])
+  end
+
+  def new_location_params
+    params[:vote].fetch(:new_locations, "").split(",")
+  end
+
+  def person_name
+    params[:vote].fetch(:person_name)
   end
 end
